4cc06f9cee1e7d50892c17f4acf971fc3be2bce5
Hrutwikbaraskar <117747747+hrutb@users.noreply.github.com>
2026-06-03 09:30:38 +0530

    digital clock

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,8 @@
 
 //Moment libraries...
 
+// const moment = require("moment/moment");
+
 
 // let a = moment() ;
   
@@ -10,30 +12,30 @@
 //  console.log(FormattedDate);
 
 
-//  import moment from 'moment'; 
-//  const moment = require('moment')
+   
+  // const moment = require('moment')
  
-let formattedDate=moment().format('YYYY-MM-DD') ;
-document.getElementById('formattedDate').textContent= formattedDate;    
+// let formattedDate=moment().format('YYYY-MM-DD') ;
+// document.getElementById('formattedDate').textContent= formattedDate;    
        
 
 
  //month date, year
- document.getElementById('yearMonth').textContent = moment().format('MMMM D , YY')
- console.log()   
+//  document.getElementById('yearMonth').textContent = moment().format('MMMM D , YY')
+//  console.log()   
 
 
 //It will give Time 
-document.getElementById('setTime').textContent = `Time  :  ${moment().format('h:mm:ss a')} `
- console.log()
+// document.getElementById('setTime').textContent = `Time  :  ${moment().format('h:mm:ss a')} `
+//  console.log()
 
 
         
 
     
    //Add 1 day to a date      
-  let date = moment('2026-03-06')
-  document.getElementById('tomorrow').textContent  =`Tommorow :  ${moment(date).add(1,'day')}`
+  // let date = moment('2026-03-06')
+  // document.getElementById('tomorrow').textContent  =`Tommorow :  ${moment(date).add(1,'day')}`
      
 
 
@@ -44,11 +46,77 @@ document.getElementById('setTime').textContent = `Time  :  ${moment().format('h:
        document.getElementById('hourAgo').textContent =`horuAgo   :  ${moment(now).subtract(1,'hour')}` ;
        
              console.log(hourAgo);
+
+
             
 //get the difference between to dates... 
-      
+       let date = moment('2026-03-06')
  let diff= moment.duration(now.diff(date));  
       
  document.getElementById('timeDiff').textContent =`The difference is     :   ${diff.hours()} hours, ${diff.minutes()} minutes , ${diff.seconds()} seconds`
-Document.write();
-             
+
+ 
+
+
+ //30 day trials calulator
+
+ const userSignUp = moment('2026-05-20');
+const today =moment();
+  console.log(today);
+  
+ document.getElementById('activeSignUp').textContent = `Active for Days :  ${today.diff(userSignUp,'days')} days`
+  
+ 
+
+
+ //Digital Clock:- 
+ let digiclock= document.getElementById('digitalClock');
+ setInterval(() => {
+         let liveclock= moment().format('HH:mm:ss A');  
+         digiclock.textContent = liveclock;
+         
+ }, 1000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
